e0a378057ed9cf023944feb72a10cb61d40a2b4d
Qrash Bandicoot <qrash@qab-lappy-02.qrashab.com>
2026-01-04 19:52:23 -0600

    v1 complete; win/lose states exist, basic mechanics exist

--- base/brick.gd
+++ base/brick.gd
@@ -1,9 +1,10 @@
 extends Area2D
 const REFLECT_X := Vector2(-1,1)
 const REFLECT_Y := Vector2(1,-1)
+signal destroy
 
 func _on_body_entered(body) -> void:
 	body.direction *= REFLECT_Y
 	queue_free()
-	body.num_bricks -= 1
+	destroy.emit()
 	pass # Replace with function body.
--- levels/lev_01.gd
+++ levels/lev_01.gd
@@ -1,7 +1,7 @@
 extends Node2D
 const REFLECT_X := Vector2(-1,1)
 const REFLECT_Y := Vector2(1,-1)
-
+var num_bricks := 8
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,7 +9,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
@@ -25,4 +25,18 @@ func _on_top_wall_body_entered(body: Node2D) -> void:
 
 func _on_right_wall_body_entered(body: Node2D) -> void:
 	body.direction *= REFLECT_X
+	pass # Replace with function body.
+
+
+func _on_bottom_wall_body_entered(body: Node2D) -> void:
+	$CanvasLayer/ShadowBox.visible = true
+	$CanvasLayer/GameOverLoseLabel.visible = true
+	pass # Replace with function body.
+
+
+func _on_brick_destroy() -> void:
+	num_bricks -= 1
+	if(num_bricks == 0):
+		$CanvasLayer/ShadowBox.visible = true
+		$CanvasLayer/GameOverWinLabel.visible = true
 	pass # Replace with function body.
